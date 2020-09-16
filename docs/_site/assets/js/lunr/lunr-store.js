dfe5af4c888af9acbfe2c70b41770947f6a50e13
Vivien WALTER <vivienwalter@viviens-imac.home>
2020-09-16 18:13:31 +0100

    Correct link error (6)

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -2,108 +2,108 @@ var store = [{
         "title": "B.Sc. in Maths, Physics & Chemistry",
         "excerpt":"Empty doc  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/aboutme/resume/2011-bsc-mpc/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/aboutme/resume/2011-bsc-mpc/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "M.Sc. in Physics & Chemistry",
         "excerpt":"Empty doc  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/aboutme/resume/2013-msc-education/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/aboutme/resume/2013-msc-education/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "M.Sc. in Physics of Materials",
         "excerpt":"Empty doc  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/aboutme/resume/2014-msc-materials/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/aboutme/resume/2014-msc-materials/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Ph.D. Student & Teaching Assistant",
         "excerpt":"Empty doc  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/aboutme/resume/2017-institut-charles-sadron/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/aboutme/resume/2017-institut-charles-sadron/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Ph.D. in Biophysics",
         "excerpt":"Empty doc  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/aboutme/resume/2017-phd-thesis/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/aboutme/resume/2017-phd-thesis/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "PostDoctoral Research Associate",
         "excerpt":"Empty doc  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/aboutme/resume/2018-institut-charles-sadron/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/aboutme/resume/2018-institut-charles-sadron/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "PostDoctoral Research Associate",
         "excerpt":"Empty doc  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/aboutme/resume/2021-kings-college-london/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/aboutme/resume/2021-kings-college-london/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Load from simulation files",
         "excerpt":"In order to analyse a simulation with ML-LPA, the simulation files should first be loaded into ML-LPA. We describe in this tutorial how to load the files. Load the files Single frame This is done using the openSystem() function. To load a single frame (no trajectory), one can use directly...","categories": ["tutorial"],
         "tags": ["file access","system"],
-        "url": "http://localhost:4000/documentation/tutorials/loading-files/1-simulation-files/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/documentation/tutorials/loading-files/1-simulation-files/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Load from position array",
         "excerpt":"In certain cases, it could be desirable to load directly the positions and configurations not from simulation files but from position arrays, as supported by NumPy. We describe in this tutorial how to proceed. Please be aware that loading a system from a position array can be a complicated and...","categories": ["tutorial"],
         "tags": ["file access","system"],
-        "url": "http://localhost:4000/documentation/tutorials/loading-files/2-positions/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/documentation/tutorials/loading-files/2-positions/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Introduction on the moments",
         "excerpt":"Content   The written content for this lesson has not been uploaded yet.   Download   The whole lesson is free to download. Use the link below to start the download (6MB).   DOWNLOAD  ","categories": ["lesson","statistics","introduction"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/lessons/statistics/1-introduction/1-moments/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/lessons/statistics/1-introduction/1-moments/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Errors and Bootstrap",
         "excerpt":"Content   The written content for this lesson has not been uploaded yet.   Download   The whole lesson is free to download. Use the link below to start the download (11MB).   DOWNLOAD  ","categories": ["lesson","statistics","introduction"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/lessons/statistics/1-introduction/2-errors/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/lessons/statistics/1-introduction/2-errors/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Linear correlation and regression",
         "excerpt":"Content   The written content for this lesson has not been uploaded yet.   Download   The whole lesson is free to download. Use the link below to start the download (2.7MB).   DOWNLOAD  ","categories": ["lesson","statistics","data fit"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/lessons/statistics/2-data-fitting/1-linear-correlation/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/lessons/statistics/2-data-fitting/1-linear-correlation/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Algorithms for data fitting",
         "excerpt":"Content   The written content for this lesson has not been uploaded yet.   Download   The whole lesson is free to download. Use the link below to start the download (5.3MB).   DOWNLOAD  ","categories": ["lesson","statistics","data fit"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/lessons/statistics/2-data-fitting/2-algorithms/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/lessons/statistics/2-data-fitting/2-algorithms/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Parametric tests",
         "excerpt":"Content   The written content for this lesson has not been uploaded yet.   Download   The whole lesson is free to download. Use the link below to start the download (8.6MB).   DOWNLOAD  ","categories": ["lesson","statistics","hypothesis tests"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/lessons/statistics/3-hypothesis-tests/1-parametric/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/lessons/statistics/3-hypothesis-tests/1-parametric/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Non-Parametric tests",
         "excerpt":"Content   The written content for this lesson has not been uploaded yet.   Download   The whole lesson is free to download. Use the link below to start the download (1.6MB).   DOWNLOAD  ","categories": ["lesson","statistics","hypothesis tests"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/lessons/statistics/3-hypothesis-tests/2-non-parametric/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/lessons/statistics/3-hypothesis-tests/2-non-parametric/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Advanced statistics",
         "excerpt":"Introduction to statistics and data analysis, from simple average and variance to fit algorithms and hypothesis tests. This lesson was made for a workshop on Python and Data Analysis that took place in 2019 at the Department of Chemistry of the King’s College London (London, UK). Content Introduction Introduction on...","categories": ["lesson","statistics"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/lessons/statistics/index/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/lessons/statistics/index/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Getting started in LabVIEW",
         "excerpt":"The tutorials on LabVIEW have not been uploaded yet. Please bear with me while I add all the documents.  ","categories": ["tutorial","labview"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/tutorials/labview/index/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/tutorials/labview/index/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       },{
         "title": "Python for Data Analysis",
         "excerpt":"The tutorials on Python have not been uploaded yet. Please bear with me while I add all the documents.  ","categories": ["tutorial","python"],
         "tags": [],
-        "url": "http://localhost:4000/teaching/tutorials/python/index/",
-        "teaser": "http://localhost:4000/assets/images/logo.png"
+        "url": "http://localhost:4000/mllpa/teaching/tutorials/python/index/",
+        "teaser": "http://localhost:4000/mllpa/assets/images/logo.png"
       }]
